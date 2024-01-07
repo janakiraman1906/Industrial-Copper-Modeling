c4af7f409f76484287e494d9c7874207d44d8595
Janakiraman V <90543107+janakiraman1906@users.noreply.github.com>
2024-01-07 11:02:53 +0530

    Copper.py

--- Copper.py
+++ Copper.py
@@ -158,4 +158,4 @@ with tab2:
         else:
             st.write('## :red[The status is Lost] ')
 
-st.write(f'<h6 style="color:rgb(0, 153, 153,0.35);">App Created by Mukesh S</h6>', unsafe_allow_html=True)
+st.write(f'<h6 style="color:rgb(0, 153, 153,0.35);">App Created by Janakiraman S</h6>', unsafe_allow_html=True)
